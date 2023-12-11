7536abcc028f25ba880eb33e96afb319458af510
Kayky Belleboni Casagrande <kayky.casagrande@e-unicred.com.br>
2023-12-11 10:40:46 -0300

    Initialize crosstream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+use crossterm::{
+    event::{read, Event},
+    terminal, ExecutableCommand,
+};
+
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    let (_w, _h) = terminal::size()?;
+
+    loop {
+        match read()? {
+            Event::Key(_key) => {}
+            _ => {}
+        }
+
+        stdout.flush()?;
+    }
 }
